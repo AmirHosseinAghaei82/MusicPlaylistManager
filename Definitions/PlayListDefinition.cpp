7c6f941ff984c78684eac6ee91c0a11fe326e36a
AmirHosseinAghaei82 <hosseinaqaei931@gmail.com>
2025-09-09 16:14:02 +0330

    Add PlayListDefinition

--- Definitions/PlayListDefinition.cpp
+++ Definitions/PlayListDefinition.cpp
@@ -3,11 +3,52 @@
 
 #include <string>
 
-using namespace std;
+#include "../Definitions/SongNodeDefinition.cpp"
 
+using namespace std;
 
 class PlayList
 {
+
+    private:
+
+        SongNode* head;
+
+        int size;
+
+    public:
+
+        PlayList() : head(nullptr), size(0) {}
+
+        SongNode* gethead()
+        {
+
+            return head;
+
+        }
+
+        int getSize()
+        {
+
+            return size;
+
+        }
+
+        void setHead(SongNode*& h)
+        {
+
+            head = h;
+
+        }
+
+
+
+
+
+
+
+
+
 
 
      
